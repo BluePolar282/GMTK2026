f67fe4e8e224304b5322b3886a9902e72de69de1
BluePolar <bluepolar282@gmail.com>
2026-07-24 18:38:31 -0500

    robot controller and world

--- README.md
+++
@@ -1,2 +0,0 @@
-# GMTK2026
-Our submission for GMTK Game Jam 2026.
---
+++ character_body_2d.gd
@@ -0,0 +1,142 @@
+extends CharacterBody2D
+
+enum STATE { 
+	FALL,
+	FLOOR, 
+	JUMP, 
+	DJUMP,
+	DASH,
+	}
+
+const SPEED = 145
+const ACCEL = 800
+const FRICTION = 650
+
+const GRAVITY = 580
+const FALL_SPEED = 300
+
+const JUMP_SPEED = -290
+const JUMP_DECEL = 850
+const JUMP_STOP = 7000
+const DJUMP_SPEED = -290
+
+const DASH_SPEED = 500
+const DASH_DECEL = 10000
+
+var active_state := STATE.FLOOR
+var playing_anim := false
+var can_DJump := false
+var can_dash := false
+var saved_position := Vector2.ZERO
+var current_dir = 1
+
+func _ready() -> void:
+	switch_state(STATE.FLOOR)
+ 
+func _physics_process(delta: float) -> void:
+	process_state(delta)
+	move_and_slide()
+	
+func movement(delta: float, input_dir: float = 0) -> void:
+	if input_dir == 0:
+		input_dir = signf(Input.get_axis("Left", "Right"))
+	var target_speed = input_dir * SPEED
+
+	if input_dir != 0:
+		current_dir = input_dir
+		velocity.x = move_toward(velocity.x, target_speed, ACCEL * get_process_delta_time())
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * get_process_delta_time())
+ 
+ 
+func switch_state(to_state: STATE) -> void:
+	var sprite = $AnimatedSprite2D
+	
+	var previous_state := active_state
+	active_state = to_state
+	# State-entry actions (run once when a state is entered)
+	match active_state:
+		
+		STATE.FALL:
+			if previous_state != STATE.DJUMP:
+				sprite.play("fall")
+			if previous_state == STATE.FLOOR:
+				$CoyoteTimer.start()
+		STATE.FLOOR:
+			can_DJump = true
+			can_dash = true
+		STATE.JUMP:
+			sprite.play("jump")
+			velocity.y = JUMP_SPEED
+			$CoyoteTimer.stop()
+			$JumpBuffer.stop()
+			
+		STATE.DJUMP:
+			sprite.play("djump")
+			velocity.y = DJUMP_SPEED
+			can_DJump = false
+			await sprite.animation_finished
+			sprite.play("fall")
+		
+		STATE.DASH:
+			sprite.play("dash")
+			velocity.y = 0
+			velocity.x = DASH_SPEED * current_dir
+			
+func process_state(delta: float) -> void:
+	var sprite = $AnimatedSprite2D
+
+	match active_state:
+		STATE.FALL:
+			velocity.y = move_toward(velocity.y, FALL_SPEED, GRAVITY * delta)
+			movement(delta)
+ 
+			if is_on_floor():
+				switch_state(STATE.FLOOR)
+				
+			if Input.is_action_just_pressed("Jump"):
+				if $CoyoteTimer.time_left > 0:
+					switch_state(STATE.JUMP)
+				elif can_DJump:
+					switch_state(STATE.DJUMP)
+				else:
+					$JumpBuffer.start()
+					
+			if Input.is_action_just_pressed("Dash"):
+				switch_state(STATE.DASH)
+				
+					
+		STATE.FLOOR:
+			if $JumpBuffer.time_left > 0:
+				switch_state(STATE.JUMP)
+				
+			if Input.get_axis("Left", "Right"):
+				sprite.play("run")
+				playing_anim = false
+			elif not playing_anim:
+				sprite.play("idle")
+			
+			if Input.is_action_just_pressed("Dash"):
+				switch_state(STATE.DASH)
+				
+			movement(delta)
+ 
+			if not is_on_floor():
+				switch_state(STATE.FALL)
+			elif Input.is_action_just_pressed("Jump"):
+				switch_state(STATE.JUMP)
+ 
+		STATE.JUMP, STATE.DJUMP:
+			velocity.y = move_toward(velocity.y, 0, JUMP_DECEL * delta)
+			if Input.is_action_just_released("Jump") or velocity.y >= 0:
+				velocity.y = move_toward(velocity.y, 0, JUMP_STOP * delta)
+				switch_state(STATE.FALL)
+			if Input.is_action_just_pressed("Dash"):
+				switch_state(STATE.DASH)
+			movement(delta)
+			
+		STATE.DASH:
+			velocity.x = move_toward(velocity.x, 0, DASH_DECEL * delta)
+			switch_state(STATE.FALL)
+			
+			
