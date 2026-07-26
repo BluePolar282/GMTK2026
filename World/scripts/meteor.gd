2333c67caf6024032d2ce4a69a5e6e44fb3682a1
BluePolar <bluepolar282@gmail.com>
2026-07-25 20:17:28 -0500

    stuff

--- World/scripts/meteor.gd
+++ World/scripts/meteor.gd
@@ -6,3 +6,7 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, 150)
 	position += velocity * delta
+
+
+func _on_despawn_timer_timeout() -> void:
+	queue_free()
