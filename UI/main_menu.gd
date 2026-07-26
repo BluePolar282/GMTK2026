dd661eb7b40bced4fff09d101de82df6fbf5a406
BluePolar <bluepolar282@gmail.com>
2026-07-26 09:07:03 -0500

    added tutorial

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://World/scenes/world.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/tutorial.tscn")
